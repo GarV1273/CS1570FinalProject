e89c1bb625b1f70015fa33d9089cb041a9b884b5
Gavin Sutherland <garvsutherland@gmail.com>
2024-04-29 18:50:27 -0500

    Started Robber Class Framework

--- robber.h
+++ robber.h
@@ -8,11 +8,36 @@
 
 #include <iostream>
 #include <string>
+#include "jewel.h"
 
 using namespace std;
 
 class Robber {
-    
+    private:
+        int id;
+        int xPos, yPos;
+        Jewel bag[20];
+        static int valueOfJewels;
+        bool isActive;
+        bool isGreedy;
+    public:
+        // Constructors
+        // Copy
+        Robber(const Robber& other);
+
+        // Desc: Picks up a piece of loot and adds it to the robbers bag
+        // Pre:  None
+        // Post: Adds the loot to the bag
+        void pickUpLoot(Jewel jewel);
+
+        // Desc: Overloads the -- operator to remove on item from the robber's bag
+        // Pre:  None
+        // Post: The last item in the bag is removed 
+        Robber operator --();
+
+        // Desc: Moves the robber one step in a direction
+        // Pre:  None
+        // Post: The robber moves to a new position and updates their position variables
 };
 
 #endif
